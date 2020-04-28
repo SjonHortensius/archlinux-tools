e24f4e0ce1e3517b8b7f0b7fc9e3e9d636d25860
Sjon Hortensius <sjon@hortensius.net>
2020-04-28 10:52:43 +0200

    move from parsing systemct-status to systemctl-show

--- service-restart-check.sh
+++ service-restart-check.sh
@@ -21,19 +21,26 @@ fi
 
 for file in /etc/systemd/system/*.wants/*.service
 do
-	# inside for-loop so they align with the columns
-	[[ -z $headerDone ]] && { echo -e '\e[1;33mPackage Upgraded Service Last-restart\e[0m'; headerDone=1; }
+	# inside for-loop so they align with the columns. Prefixed with newline to prevent first column from not being wide enough
+	[[ -z $headerDone ]] && { echo -e '\e[1;33m\nPackage\tUpgraded\tService\tLast-restart\e[0m'; headerDone=1; }
 
-	service=$(readlink $file 2>/dev/null)
-	package=$(pacman -Qoq $service) || continue
+	service=$(readlink $file)
+
+	# services in /etc are machine-specific and not owned by a pkg
+	[[ $service == /etc/systemd/system/*.service ]] && continue
+
+	package=$(pacman -Qoq $service)
 	file=${file##*/}
 
-	started=$(systemctl status -n0 $file | grep Active: | cut -d' ' -f8-10)
+	# don't restart nspawns
+	[[ $service == '/usr/lib/systemd/system/systemd-nspawn@.service' ]] && continue
+
+	started=$(systemctl show -p ActiveEnterTimestamp $file | cut -d= -f2)
 	updated=$(pacman -Qi $package | grep '^Install Date' | cut -d: -f2-)
 
 	[[ $(date -d "$started" +%s) -lt $(date -d "$updated" +%s) ]] || continue
 
-	[[ $1 == '--now' ]] && systemctl restart $file && echo -n '* '
+	[[ $1 == '--now' ]] && systemctl restart $file
 
-	echo $package $(date -d "$updated" +'%Y-%m-%d:%H:%M:%S') $file $(date -d "$started" +'%Y-%m-%d:%H:%M:%S')
-done|column -t
+	printf '%s\t%s\t%s\t%s\n' $package $(date -d "$updated" +'%Y-%m-%d:%H:%M:%S') $file $(date -d "$started" +'%Y-%m-%d:%H:%M:%S')
+done | column -ts $'\t'
